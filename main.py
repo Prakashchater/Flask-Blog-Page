3d5e59d43428de17a91e4103bfd2cc44eb52bd17
Prakash <prakashchater@gmail.com>
2021-05-26 20:56:10 +0530

    Adding SQLAlchemy to run locally

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ email = "chaterprakash@gmail.com"
 password = "pcchater@160997"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "qwertyuioplkjhgfdsa"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] ="sqlite:///blog.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
